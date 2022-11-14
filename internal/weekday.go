f21dbaecde247afd61b3528b48ce87e36451d8cf
AMARLG <adrian.marszalek@scania.com>
2022-11-14 16:56:44 +0100

    weekdays code improvement

--- internal/weekday.go
+++ internal/weekday.go
@@ -4,13 +4,13 @@ type Weekday int8
 type WeekdaysSum int8
 
 const (
-	Monday    Weekday = 1
-	Tuesday   Weekday = 2
-	Wednesday Weekday = 4
-	Thursday  Weekday = 8
-	Friday    Weekday = 16
-	Saturday  Weekday = 32
-	Sunday    Weekday = 64
+	Monday Weekday = 1 << iota
+	Tuesday
+	Wednesday
+	Thursday
+	Friday
+	Saturday
+	Sunday
 )
 
 func (sum WeekdaysSum) AsWeekdays() []Weekday {
