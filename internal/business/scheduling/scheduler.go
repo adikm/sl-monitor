264e61d0d0d9a207638646a79d2a7a4f0601310e
AMARLG <adrian.marszalek@scania.com>
2023-01-09 14:28:40 +0100

    #3 TestScheduler_ScheduleNotifications

--- internal/business/scheduling/scheduler.go
+++ internal/business/scheduling/scheduler.go
@@ -2,6 +2,7 @@ package scheduling
 
 import (
 	"fmt"
+	"log"
 	"sl-monitor/internal"
 	"sl-monitor/internal/business/notifications"
 	"sync"
@@ -12,59 +13,62 @@ type Scheduler struct {
 	Service notifications.Service
 }
 
-type result struct {
-	success bool
-	no      notifications.Notification
+type Result struct {
+	success        bool
+	notificationId int
 }
 
-// ScheduleNotifications fanin fanout pattern
-func (s *Scheduler) ScheduleNotifications() {
+// ScheduleNotifications fanin fanout pattern.
+// this method is blocking
+func (s *Scheduler) ScheduleNotifications() []Result {
 	today := s.today()
 	notificationsToSchedule, err := s.Service.FindAllForWeekday(today)
 	if err != nil {
-
+		log.Println(err)
+		return nil
 	}
 
-	var results []result
+	var results []Result
 	var wg sync.WaitGroup
 	for _, n := range *notificationsToSchedule {
 		now := time.Now()
 		executionDate := time.Date(now.Year(), now.Month(), now.Day(), n.Timestamp.Hour(), n.Timestamp.Minute(), n.Timestamp.Second(), 0, time.Local) // always today and time that's saved in db
-		resultChan := make(chan result, 1)
+		resultChan := make(chan Result, 1)
 		wg.Add(1)
 		go s.scheduleNotification(n, executionDate, &wg, resultChan)
 		wg.Add(1)
 		go s.collectResult(&results, resultChan, &wg)
 	}
 	wg.Wait()
-	fmt.Println(results)
+	fmt.Printf("Finished with results %v \r\n", results)
 
-	fmt.Println("finito")
+	return results
 }
 
-func (s *Scheduler) scheduleNotification(n notifications.Notification, until time.Time, wg *sync.WaitGroup, sampleChan chan result) {
+func (s *Scheduler) scheduleNotification(n notifications.Notification, until time.Time, wg *sync.WaitGroup, sampleChan chan Result) {
 	defer wg.Done()
 
 	defer close(sampleChan)
 	var w sync.WaitGroup
 	w.Add(1)
-	fmt.Printf("starting and waiting until %s \r\n", until)
+	fmt.Printf("Scheduled notification id=%d on %s \r\n", n.Id, until)
 	time.Sleep(time.Until(until))
 	go s.sendNotification(n, sampleChan, &w)
 	w.Wait()
 }
 
-func (s *Scheduler) collectResult(results *[]result, resultChan chan result, wg *sync.WaitGroup) { // consumer
+func (s *Scheduler) collectResult(results *[]Result, resultChan chan Result, wg *sync.WaitGroup) { // consumer
 	defer wg.Done()
 	for s := range resultChan {
 		*results = append(*results, s)
 	}
 }
 
-func (s *Scheduler) sendNotification(n notifications.Notification, channel chan<- result, wg *sync.WaitGroup) { // producer
+func (s *Scheduler) sendNotification(n notifications.Notification, channel chan<- Result, wg *sync.WaitGroup) { // producer
 	defer wg.Done()
+	fmt.Printf("Sending notification id =%d \r\n", n.Id)
 	fmt.Printf("EMAILED!! %v \r\n", n) // email it
-	channel <- result{success: true, no: n}
+	channel <- Result{success: true, notificationId: n.Id}
 }
 
 func (s *Scheduler) today() internal.Weekday {
--- internal/business/scheduling/scheduler_test.go
+++ internal/business/scheduling/scheduler_test.go
@@ -1,11 +1,22 @@
 package scheduling
 
 import (
+	"fmt"
 	"sl-monitor/internal/business/notifications"
 	"testing"
 )
 
-func TestScheduler_DoIt(t *testing.T) {
+func TestScheduler_ScheduleNotifications(t *testing.T) {
 	scheduler := Scheduler{Service: &notifications.NotificationServiceStub{}}
-	scheduler.ScheduleNotifications()
+	got := scheduler.ScheduleNotifications()
+
+	fmt.Printf("%v", got)
+	if len(got) != 3 {
+		t.Errorf("ScheduleNotifications() returned wrong length got = %v, want %v", len(got), 3)
+	}
+	for _, result := range got {
+		if result.success != true {
+			t.Errorf("ScheduleNotifications() returned success=false for notificationId=%v", result.notificationId)
+		}
+	}
 }
